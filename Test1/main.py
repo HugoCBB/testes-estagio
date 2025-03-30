51f658622221e89b526d01512858ed3328fb53c3
HugoCBB <hugocarlosb.071@gmail.com>
2025-03-30 20:06:30 -0300

    Teste 1 - Completo

---
+++ Test1/main.py
@@ -0,0 +1,6 @@
+from scraper.scraper import PDF
+
+client = PDF("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos")
+client.ExtrairPDF()
+client.BaixarPDF()
+client.CompactarZIP()
